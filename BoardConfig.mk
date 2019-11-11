970c38a2c353e1daa40637b514be13eb12a643b0
ShivamKumarJha <jha.shivam3@gmail.com>
2019-11-11 00:58:29 +0000

    s7182e_2g-user 8.1.0 OPM2.171019.012 22418 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/dexp/N570
+BOARD_VENDOR := dexp
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/dexp/N570/BoardConfigVendor.mk
